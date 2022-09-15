f4c407368ee6df5ef1cd54b647e4823cb7893f41
ArteMaxL <artemiolucero@gmail.com>
2022-09-15 13:52:16 -0300

    Se eliminaron using no utilizados

--- EjercicioPooTransporte/EjercicioPooTransporte/Omnibus.cs
+++ EjercicioPooTransporte/EjercicioPooTransporte/Omnibus.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace EjercicioPooTransporte
+﻿namespace EjercicioPooTransporte
 {
     public class Omnibus : TransportePublico
     {
