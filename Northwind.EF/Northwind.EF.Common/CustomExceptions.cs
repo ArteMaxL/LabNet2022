ba1c0acdf64258a08ca9e49a48893bfd5b835e9e
ArteMaxL <artemiolucero@gmail.com>
2022-09-25 03:35:08 -0300

    Added Validations and Custom Exceptions

---
+++ Northwind.EF/Northwind.EF.Common/CustomExceptions.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Northwind.EF.Common
+{
+    public class CustomExceptions : Exception
+    {
+        public static List<String> CustomException(Exception e)
+        {
+            var customMessage = $"Exception type: {e.GetType()}";            
+            List<String> result = new List<String>() { customMessage, e.Message };
+            return result;
+        }
+
+        public static List<String> CustomFormatException(FormatException e)
+        {
+            var customMessage = $"Exception type: {e.GetType()}";
+            List<String> result = new List<String>() { customMessage, e.Message };
+            return result;
+        }
+        public static List<String> CustomOverflowException(OverflowException e)
+        {
+            var customMessage = $"Exception type: {e.GetType()}";
+            List<String> result = new List<String>() { customMessage, e.Message };
+            return result;
+        }
+
+        public static List<String> CustomNullReferenceException(NullReferenceException e)
+        {
+            var customMessage = $"Exception type: {e.GetType()}";
+            List<String> result = new List<String>() { customMessage, e.Message };
+            return result;
+        }
+    }
+}
---
+++ Northwind.EF/Northwind.EF.Common/Validation.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Northwind.EF.Common
+{
+    public class Validation
+    {
+        public bool IsValid(string input)
+        {
+            try
+            {
+                var validInput = int.Parse(input);
+                return true;
+            }
+            catch (FormatException e)
+            {
+                CustomExceptions.CustomFormatException(e);                
+                return false;
+            }
+            catch (Exception e)
+            {
+                CustomExceptions.CustomException(e);
+                return false;
+            }
+        }
+
+        public bool CategoryLong(string input)
+        {
+            int limit = 15;
+            int count = input.Length;
+            
+            if (count <= limit) return true;
+            else return false;
+        }
+    }
+}
