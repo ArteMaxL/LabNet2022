27008cd43e0dc9fe2a03c979a2d32b696b608990
ArteMaxL <artemiolucero@gmail.com>
2022-09-30 14:25:47 -0300

    Rename Project

--- Northwind.EF/Northwind.EF.Logic/CustomerLogic.cs
+++ Northwind.EF/Northwind.EF.Logic/CustomerLogic.cs
@@ -31,6 +31,7 @@ namespace Northwind.EF.Logic
             _context.SaveChanges();
         }
 
+        // Ejercicio 6 ??
         public override List<Customers> GetAll()
         {
             return _context.Customers.ToList();
@@ -41,12 +42,6 @@ namespace Northwind.EF.Logic
             return _context.Customers.Find(id);
         }
 
-        public Customers GetOneString(string id)
-        {
-            var customer = _context.Customers.Single(c => c.CustomerID == id);
-            return customer;
-        }
-
         public override void Update(Customers customerNew)
         {
             var customerUpdate = _context.Customers.Find(customerNew.CustomerID);
@@ -55,6 +50,47 @@ namespace Northwind.EF.Logic
             customerUpdate.ContactName = customerNew.ContactName;            
 
             _context.SaveChanges();
+        }
+
+        // Modified
+        public Customers GetOneString(string id)
+        {
+            var customer = _context.Customers.Where(c => c.CustomerID == id)
+                                             .SingleOrDefault();
+
+            return (customer != null)? customer : throw new NullReferenceException(); //TODO
+        }
+
+        public List<Customers> GetCustomersRegionWA()
+        {
+            var customers = _context.Customers.Where(c => c.Region == "WA")
+                                              .ToList();
+            return customers;
+        }
+
+        public IEnumerable<CustomersOrders> CustomersJoinOrdersRegionWADate1997()
+        {
+            DateTime date = DateTime.ParseExact(19970101.ToString(), "yyyyMMdd", null);
+
+            var customersOrders = from Customers in _context.Customers
+                                  join Orders in _context.Orders
+                                  on Customers.CustomerID equals Orders.CustomerID
+                                  where Orders.OrderDate == date && Customers.Region == "WA"
+                                  select new CustomersOrders
+                                  {
+                                      CustomerID = Customers.CustomerID,
+                                      CompanyName = Customers.CompanyName,
+                                      OrderID = Orders.OrderID,
+                                  };
+            return customersOrders;
+        }
+
+        public List<Customers> GetFirst3CustomersRegionWA()
+        {
+            var customers = _context.Customers.Where(c => c.Region == "WA")
+                                              .Take(3)
+                                              .ToList();
+            return customers;
         }
     }
 }
---
+++ Northwind.EF/Northwind.EF.Logic/ProductLogic.cs
@@ -0,0 +1,78 @@
+﻿using Northwind.EF.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Northwind.EF.Logic
+{
+    internal class ProductLogic : BaseLogic<Products>
+    {
+
+        // Metodos Heredados 
+        public override void Add(Products newT)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override List<Products> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Products GetOne(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Update(Products updateT)
+        {
+            throw new NotImplementedException();
+        }
+
+        // Metodos Especificos TP 5
+
+        public List<Products> GetProductsWhitoutStock()
+        {
+            var products = _context.Products.Where(p => p.UnitsInStock == 0)
+                                            .ToList();
+            return products;
+        }
+
+        public List<Products> GetProductsWhithStockPriceUpTo3()
+        {
+            var products = _context.Products.Where(p => p.UnitsInStock > 0 && p.UnitPrice > 3)
+                                            .ToList();
+            return products;
+        }
+
+        public Products GetProductWithId789()
+        {
+            var product = _context.Products.Where(p => p.ProductID == 789)
+                                           .FirstOrDefault();
+            return product;
+        }
+
+        public List<Products> GetProductsOrderByName()
+        {
+            var products = _context.Products.OrderBy(p => p.ProductName)
+                                            .ToList();
+            return products;
+        }
+        
+        public List<Products> GetProductsOrderByUnitStockMajorToMinor()
+        {
+            var products = from product in _context.Products
+                            orderby product.UnitsInStock descending
+                            select product;
+
+            return products.ToList();
+        }
+    }
+}
