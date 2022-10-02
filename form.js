cdc3786c0a58c949b2d3c51f741bdc77b36bb77a
ArteMaxL <artemiolucero@gmail.com>
2022-10-02 03:06:01 -0300

    Added script

---
+++ form.js
@@ -0,0 +1,29 @@
+const form = document.getElementById("form");
+const nombre = document.getElementById("nombre");
+const apellido = document.getElementById("apellido");
+const edad = document.getElementById("edad");
+const parrafo = document.getElementById("warnings");
+
+form.addEventListener("submit", e =>{
+    e.preventDefault()
+    let warnings = "";
+    let entrar = false;
+    parrafo.innerHTML = "";
+
+    if(nombre.value.trim() == null || nombre.value.trim() == "" || nombre.value.trim().length < 2){
+        warnings += `El Nombre no es válido<br>`
+        entrar = true
+    }
+    if(apellido.value.trim() == null || apellido.value.trim() == "" || apellido.value.trim().length < 4){
+        warnings += `El Apellido no es válido<br>`
+        entrar = true
+    }
+    
+    
+
+    if(entrar){
+        parrafo.innerHTML = warnings
+    }else{
+        parrafo.innerHTML = "Formulario Enviado!"
+    }
+});
