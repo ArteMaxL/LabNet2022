eeea66f17ea4587a479ccaa54e428cb277a240a7
ArteMaxL <artemiolucero@gmail.com>
2022-10-08 17:03:17 -0300

    Added FakeStore Logic

---
+++ Northwind.EF/Northwind.EF.Logic/FakeStoreLogic.cs
@@ -0,0 +1,37 @@
+﻿using Newtonsoft.Json;
+using Northwind.EF.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Northwind.EF.Logic
+{
+    public class FakeStoreLogic
+    {
+        public async Task<IQueryable<FakeStoreAPI>> GetProducts()
+        {
+            string apiUrl = "https://fakestoreapi.com/products";
+            HttpClient httpClient = new HttpClient();
+
+            try
+            {
+                string responseJson = await httpClient.GetStringAsync(apiUrl);
+
+                IQueryable<FakeStoreAPI> products = JsonConvert.
+                    DeserializeObject<IQueryable<FakeStoreAPI>>(responseJson);
+
+                return products;
+
+            }
+            catch (Exception ex)
+            {
+
+                throw new Exception(ex.Message);
+            }
+
+        }
+    }
+}
