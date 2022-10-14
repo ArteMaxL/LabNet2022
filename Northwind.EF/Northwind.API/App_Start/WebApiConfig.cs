fc54b3aa187613c471f6e50379aa0e68e62ea15c
ArteMaxL <artemiolucero@gmail.com>
2022-10-14 20:21:44 -0300

    Installed CORS

--- Northwind.EF/Northwind.API/App_Start/WebApiConfig.cs
+++ Northwind.EF/Northwind.API/App_Start/WebApiConfig.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
+using System.Web.Http.Cors;
 
 namespace Northwind.API
 {
@@ -10,6 +11,10 @@ namespace Northwind.API
         public static void Register(HttpConfiguration config)
         {
             // Web API configuration and services
+            EnableCorsAttribute cors = new EnableCorsAttribute(
+                origins: "https://localhost:44337,http://localhost:4200", headers: "*", methods: "*");
+
+            config.EnableCors(cors);
 
             // Web API routes
             config.MapHttpAttributeRoutes();
