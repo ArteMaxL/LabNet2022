db554a71363ccf5422936359793e269a23bfcbb0
ArteMaxL <artemiolucero@gmail.com>
2022-10-15 17:08:05 -0300

    Added routing

--- Northwind.EF/Northwind.Angular.UI/src/app/app-routing.module.ts
+++ Northwind.EF/Northwind.Angular.UI/src/app/app-routing.module.ts
@@ -1,9 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ShipperFormComponent } from './modules/shippers/components/shipper-form/shipper-form.component';
+import { HomeComponent } from './layout/home/home.component';
 
 const routes: Routes = [{
     path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'shippers',
     component: ShipperFormComponent
   }
 ];
