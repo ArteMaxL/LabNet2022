15e8021cb95337221f95b0438449cee430b03a17
ArteMaxL <artemiolucero@gmail.com>
2022-10-14 23:56:35 -0300

    Instaled dependencies

--- Northwind.EF/Northwind.Angular.UI/src/app/app.module.ts
+++ Northwind.EF/Northwind.Angular.UI/src/app/app.module.ts
@@ -1,20 +1,33 @@
+import { RouterModule, Routes } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { ModalModule, BsModalRef } from 'ngx-bootstrap/modal';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { CrudShippersModule } from './modules/crud-shippers/crud-shippers.module';
+
+
 @NgModule({
   declarations: [
     AppComponent
+
   ],
   imports: [
+
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    CrudShippersModule,
+    HttpClientModule,
+    ModalModule.forRoot()
+
   ],
-  providers: [],
+  providers: [BsModalRef],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
